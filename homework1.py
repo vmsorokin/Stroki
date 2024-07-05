0c5ecdf4c997fb5e6f6b753eb4d57920f2dea7c3
Vladimir <sorokin-v-m@yandex.ru>
2024-07-05 20:27:42 +0500

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,8 @@
+#1st prohram stroki
+example='Как в этом, разобраться!?'
+print(example[0])
+print(example[-1])
+print(example[12:])
+print(example[::-1])
+print(example[::2])
+#print(len(example)) # для себя чтобы не считать символы
