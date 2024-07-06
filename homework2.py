d7b749985294200c4be6802a69175e1805321966
Vladimir <sorokin-v-m@yandex.ru>
2024-07-06 22:29:35 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+homework=12
+time_homework=1.5
+curse='Python'
+print('Курс:',curse,'всего задач:',homework,'затрачено часов:',time_homework, 'среднее время выполнения задачи', time_homework/homework*60, 'минут.')
