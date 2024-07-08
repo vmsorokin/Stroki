b672726ef7cbcffa9229fc180f3e06c9b9ec7fe7
Vladimir <sorokin-v-m@yandex.ru>
2024-07-08 22:50:15 +0500

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+#1st_program
+my_string=input('Расскажите, пожалуйста как прошел ваш день? ')
+print('Вы ввели:', int(len(my_string)), 'символа(ов)')
+#2st_program
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
